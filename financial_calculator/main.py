00697078498a9696cd22fddcd434d4246a873d85
ConnorP4231 <connor.pavicic@ucas-edu.net>
2025-01-10 13:17:41 -0700

    Finished compound interest and started on allocator

--- financial_calculator/main.py
+++ financial_calculator/main.py
@@ -5,18 +5,32 @@
 # deposit = int(input('Enter the number that you will deposit each month/week: '))
 # deposit_rate = input('Will you deposit money every week or month?: ').lower()
 
-compound_inital = int(input('How much money will you originally start with?: '))
-compound_increase = (int(input('What percent do you want to have your money increase by?: '))/100)
-compound_frequency = int(input('How many times per year will you receive money?: '))
-compound_time = int(input('How many years do you want to want to have this going?: '))
+# compound_inital = int(input('How much money will you originally start with?: '))
+# compound_increase = (int(input('What percent do you want to have your money increase by?: '))/100)
+# compound_frequency = int(input('How many times per year will you receive money?: '))
+# compound_time = int(input('How many years do you want to want to have this going?: '))
 
 # def saving_goal(goal=goal, deposit=deposit):
     # time = goal/deposit
     # return time
 
-def compound_interest(compound_inital = compound_inital, compound_increase = compound_increase, compound_time = compound_time):
-    final_amount = (compound_inital*(((1+(compound_increase/compound_time))^(compound_frequency*compound_time)))) #formula is not working
-    return final_amount
+# def compound_interest(compound_inital = compound_inital, compound_increase = compound_increase, compound_time = compound_time):
+    # final_amount = compound_inital*((1+(compound_increase/compound_frequency))**(compound_frequency*compound_time))
+    # return final_amount
 
 
-print(compound_interest())
+# print(compound_interest())
+
+allocator_categories = []
+
+while True:
+    allocator_add = input('What category would you like to add to the different categories you will put your money in?: ').lower()
+    allocator_categories.append(allocator_add)
+
+    keep_going = input('Do you want to add another category? (yes or no): ')
+    if keep_going == 'yes':
+        continue
+    elif keep_going == 'no':
+        break
+    else:
+        continue
