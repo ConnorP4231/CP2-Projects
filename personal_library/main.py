773de5a60c300a4d8951e43243cf6ad3d90056d5
ConnorP4231 <connor.pavicic@ucas-edu.net>
2025-01-14 13:13:09 -0700

    created a folder for personal library

---
+++ personal_library/main.py
@@ -0,0 +1,8 @@
+# Connor Pavicic, personal_library
+
+# What the program will do:
+# Have users add different music artists to the set
+# Have users remove different music artists
+# Have users search for artists by genre, name, or a song name
+
+set = {}
