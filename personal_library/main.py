e708b475c39176e71fb2a3c1a908debb0ba6c8f7
ConnorP4231 <connor.pavicic@ucas-edu.net>
2025-01-15 13:44:04 -0700

    didnt really do much

--- personal_library/main.py
+++ personal_library/main.py
@@ -5,4 +5,17 @@
 # Have users remove different music artists
 # Have users search for artists by genre, name, or a song name
 
-set = {}
+artists = {}
+
+def add_artist():
+    for x in range(2):
+        artist = input('Enter an artist name that you would like to enter: ')
+        genre = input('Enter the genre that their music style is: ')
+        song = input('Enter a specific song of theirs if you want to search them easier. (Leave this blank if you do not want to.): ')
+
+        artist_bio = (artist, genre, song)
+
+    artists.add(artist_bio)
+    print(artists)
+
+add_artist()
