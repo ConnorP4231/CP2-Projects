d0dc0a67095b7d72386b331fbbf9e9a2e37c9e10
ConnorP4231 <connor.pavicic@ucas-edu.net>
2025-01-17 13:44:09 -0700

    i understand it now

--- personal_library/main.py
+++ personal_library/main.py
@@ -7,20 +7,42 @@
 
 artists = []
 
-artist = input('Enter an artist: ')
-genre = input('Enter a genre: ')
-artist_bio = ('Artist:', artist, ', Genre:', genre)
-artist_bio = ' '.join(artist_bio)
-artists.append(artist_bio)
+def add_artist(artists=artists):
+    artist = input('Enter an artist: ').lower()
+    genre = input('Enter a genre: ').lower()
+    artist_bio = ('Artist:', artist, ', Genre:', genre)
+    artist_bio = ' '.join(artist_bio)
+    artists.append(artist_bio)
 
-print(artists)
+def search():
+    search_list = []
+    search = input('Enter an artist/genre to search for: ')
 
-search = input('Enter an artist to search for: ').lower()
-
-search_list = []
-for i in artists:
-    if search in i:
-        search_list.append(i)
-
-if not search_list:
+    for x in artists:
+        if search in x:
+            search_list.append(x)
     
+    if not search_list:
+        print('Artist/Genre not found.')
+    elif search_list:
+        print(search_list)
+
+def remove_artist(artists=artists):
+    remove = input('Enter an artist that you would like to remove from the list: ')
+    
+    remove_text = ''
+    for x in artists:
+        if search in x:
+            remove_text += x
+    
+    if not remove_text:
+        print('Artist/Genre not found.')
+    elif remove_text:
+        print(f'Are you sure you want to remove this artist: {remove_text}')
+    
+
+def main():
+    add_artist()
+    search()
+
+main()
