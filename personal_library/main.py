48cba9112a2bd4e9a4a586d67ed9b4bdd6d247c8
ConnorP4231 <connor.pavicic@ucas-edu.net>
2025-01-17 13:13:03 -0700

    fixes

--- personal_library/main.py
+++ personal_library/main.py
@@ -7,10 +7,10 @@
 
 artists = []
 
-artist = input('Enter an artist: ')
-genre = input('Enter a genre: ')
+for x in range(2):
+    artist = input('Enter an artist: ')
+    genre = input('Enter a genre: ')
+    artist_bio = {'Artist:', artist, 'Genre:', genre}
+    artist_bio = ' '.join(artist_bio)
+    artists.append(artist_bio)
 
-artist_bio = {artist, genre}
-artist_bio = ' '.join(artist_bio)
-
-print(artists)
