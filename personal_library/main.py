06f28ccab7c8162fdad44aaa136f9de93e9d85f2
ConnorP4231 <connor.pavicic@ucas-edu.net>
2025-01-21 13:43:52 -0700

    did a lot of coding stuff

--- personal_library/main.py
+++ personal_library/main.py
@@ -13,6 +13,7 @@ def add_artist(artists=artists):
     artist_bio = ('Artist:', artist, ', Genre:', genre)
     artist_bio = ' '.join(artist_bio)
     artists.append(artist_bio)
+    print('Artist added!')
 
 def search():
     search_list = []
@@ -30,19 +31,46 @@ def search():
 def remove_artist(artists=artists):
     remove = input('Enter an artist that you would like to remove from the list: ')
     
-    remove_text = ''
+    remove_list = []
     for x in artists:
-        if search in x:
-            remove_text += x
+        if remove in x:
+            remove_list += x
     
-    if not remove_text:
+    if not remove_list:
         print('Artist/Genre not found.')
-    elif remove_text:
-        print(f'Are you sure you want to remove this artist: {remove_text}')
+    elif remove_list:
+        while len(remove_list) > 1:
+            remove_again = input(f'Which one of these artists: {remove_list} would you like to remove: ')
+            remove_list = []
+            for x in artists:
+                if remove_again in x:
+                    remove_list += x    
+        
+        str(remove_list)
+        are_you_sure = input(f'Are you sure that you want to remove this artist: {remove_list}').lower()
+        
+        if are_you_sure == 'yes':
+            artists.remove(remove_list)
+            print('Artist deleted.')
+        elif are_you_sure == 'no':
+            pass
+        else:
+            print('Incorrect option. The artist is still in the list.')
     
 
 def main():
-    add_artist()
-    search()
+    options = input("""
+Options: 
+
+1. Add an artist
+2. Remove an artist
+3. Search for an artist
+
+Which one would you like to do? (1, 2, or 3): """)
+
+    if options == 1:
+        add_artist()
+    elif options == 2
+
 
 main()
