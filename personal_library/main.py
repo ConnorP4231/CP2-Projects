29759806c6025c348391defdb2e4d9c2a1f1bf59
ConnorP4231 <connor.pavicic@ucas-edu.net>
2025-01-21 18:34:57 -0700

    finished personal library

--- personal_library/main.py
+++ personal_library/main.py
@@ -5,21 +5,21 @@
 # Have users remove different music artists
 # Have users search for artists by genre or name
 
-artists = []
+artists = [] # The main list that stores the artists
 
-def add_artist(artists=artists):
+def add_artist(artists=artists): # The function that adds an artist to the list
     artist = input('Enter an artist: ').lower()
     genre = input('Enter a genre: ').lower()
-    artist_bio = ('Artist:', artist, ', Genre:', genre)
+    artist_bio = ('Artist:', artist, ', Genre:', genre) #This is the tuple that I used.
     artist_bio = ' '.join(artist_bio)
     artists.append(artist_bio)
     print('Artist added!')
 
-def search():
+def search(artists=artists): #This is the function to search for the artists.
     search_list = []
     search = input('Enter an artist/genre to search for: ')
 
-    for x in artists:
+    for x in artists: #Goes through each value of the artist list and if it is equal to the search then it puts it into a new list.
         if search in x:
             search_list.append(x)
     
@@ -28,37 +28,41 @@ def search():
     elif search_list:
         print(search_list)
 
-def remove_artist(artists=artists):
-    remove = input('Enter an artist that you would like to remove from the list: ')
+def remove_artist(artists=artists): #This is the function to remove an artist.
+    remove = input('Enter an artist that you would like to remove from the list (you can search their genre to identify them): ').lower()
+
     
-    remove_list = []
-    for x in artists:
-        if remove in x:
-            remove_list += x
-    
+    remove_list = [x for x in artists if remove in x.lower()] # A little bit different style of code since I have to remove the artist.
+
     if not remove_list:
         print('Artist/Genre not found.')
-    elif remove_list:
-        while len(remove_list) > 1:
-            remove_again = input(f'Which one of these artists: {remove_list} would you like to remove: ')
-            remove_list = []
-            for x in artists:
-                if remove_again in x:
-                    remove_list += x    
-        
-        str(remove_list)
-        are_you_sure = input(f'Are you sure that you want to remove this artist: {remove_list}').lower()
-        
-        if are_you_sure == 'yes':
-            artists.remove(remove_list)
-            print('Artist deleted.')
-        elif are_you_sure == 'no':
-            pass
-        else:
-            print('Incorrect option. The artist is still in the list.')
+        return
+
+    # If their search pulls up multiple artists: 
+    while len(remove_list) > 1:
+        print(f'Multiple matches found: {remove_list}')
+        remove_again = input('Please specify the exact artist you want to remove: ').lower()
+        remove_list = [x for x in remove_list if remove_again in x.lower()]
+
+        if not remove_list:
+            print('No matches found for your input. Exiting removal process.')
+            return
+
+    
+    artist_to_remove = remove_list[0]
+    are_you_sure = input(f'Are you sure that you want to remove this artist: "{artist_to_remove}"? (yes/no): ').lower() #Makes sure the user actually wants to remove the specified artist.
+
+    if are_you_sure == 'yes':
+        artists.remove(artist_to_remove)
+        print('Artist deleted.')
+    elif are_you_sure == 'no':
+        print('Operation canceled. The artist was not removed.')
+    else:
+        print('Invalid option. The artist is still in the list.')
+
     
 
-def main():
+def main(): #The main function that runs all the other functions based off what the user wants.
     while True:
         options = input("""
 Options: 
@@ -66,7 +70,8 @@ Options:
 1. Add an artist
 2. Remove an artist
 3. Search for an artist
-4. Exit the program
+4. Look at the artist list
+5. Exit the program
 
 Which one would you like to do? (1, 2, 3, or 4): """)
 
@@ -76,7 +81,9 @@ Which one would you like to do? (1, 2, 3, or 4): """)
             remove_artist()
         elif options == '3':
             search()
-        elif options == '4':
+        elif options == '4': #They can look at their artist list.
+            print(artists)
+        elif options == '5': #They can exit the program
             print('Thanks for using the program!')
             break
         else:
