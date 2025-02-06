7d99d49c9777247f6c4d020cc14cef0325b792a2
ConnorP4231 <connor.pavicic@ucas-edu.net>
2025-02-05 19:23:13 -0700

    my bad

--- test/main.py
+++ test/main.py
@@ -1,83 +1,76 @@
 # Connor Pavicic, morse_code_translator
 
-translation_dict = {
-    'a': '.-',    'b': '-...',  'c': '-.-.',  'd': '-..',   'e': '.', 
-    'f': '..-.',  'g': '--.',   'h': '....',  'i': '..',    'j': '.---', 
-    'k': '-.-',   'l': '.-..',  'm': '--',    'n': '-.',    'o': '---', 
-    'p': '.--.',  'q': '--.-',  'r': '.-.',   's': '...',  't': '-', 
-    'u': '..-',   'v': '...-', 'w': '.--',   'x': '-..-',  'y': '-.--', 
-    'z': '--..',  
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
+           'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
+           'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', 
+           '4', '5', '6', '7', '8', '9']
 
-    '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', 
-    '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.'
-}
-
-reversed_translation_dict = {
-    '.-': 'a',   '-...': 'b',   '-.-.': 'c',   '-..': 'd',   '.': 'e', 
-    '..-.': 'f', '--.': 'g',   '....': 'h',   '..': 'i',    '.---': 'j', 
-    '-.-': 'k',  '.-..': 'l',  '--': 'm',    '-.': 'n',    '---': 'o', 
-    '.--.': 'p', '--.-': 'q',  '.-.': 'r',   '...': 's',   '-': 't', 
-    '..-': 'u',  '...-': 'v',  '.--': 'w',   '-..-': 'x',  '-.--': 'y', 
-    '--..': 'z',  
-
-    '-----': '0', '.----': '1', '..---': '2', '...--': '3', '....-': '4', 
-    '.....': '5', '-....': '6', '--...': '7', '---..': '8', '----.': '9'
-}
-
+morse_codes = [
+    '.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', 
+    '-.-', '.-..', '--', '-.', '---', '.--.', '--.-', '.-.', '...', '-', 
+    '..-', '...-', '.--', '-..-', '-.--', '--..',
+    '-----', '.----', '..---', '...--', '....-', '.....', '-....', '--...', '---..', '----.'
+]
 
 def english_to_morse():
-    valid_chars = "abcdefghijklmnopqrstuvwxyz1234567890 "
-    while True:
-        to_be_translated = input('What do you want to translate into Morse code? (Only letters, digits, and spaces allowed): ')
+    valid = False
+    valid_chars = letters + [' ']
+    while not valid:
+        to_be_translated = input('What do you want to translate into morse code?: ').lower()
         if all(char in valid_chars for char in to_be_translated):
-            break
+            valid = True
         else:
-            print('Invalid characters found. Please only use letters, digits, and spaces.')
+            print('Invalid characters found, try again.')
     
     final_translation = ''
-
-    for x in to_be_translated:
-        if x in translation_dict:
-            final_translation += translation_dict[x] + ' '
-        elif x == ' ':
-            final_translation += '/ '  # This represents space in Morse code
-
-    print("Morse Code Translation:", final_translation.strip())
-
+    
+    for char in to_be_translated:
+        if char in letters:
+            final_translation += morse_codes[letters.index(char)] + ' '
+        elif char == ' ':
+            final_translation += '/ '
+    
+    print(final_translation)
 
 def morse_to_english():
-    valid_chars = './- '
-    while True:
-        to_be_translated = input('What do you want to translate into English? (Only Morse code symbols ./- and spaces allowed): ')
-        # Check that all characters in the input are valid Morse symbols or spaces
+    valid = False
+    valid_chars = ['.', '-', '/', ' ']
+    while not valid:
+        to_be_translated = input('What do you want to translate into English?: ')
         if all(char in valid_chars for char in to_be_translated):
+            valid = True
+        else:
+            print('Invalid characters found, try again.')
+    
+    final_translation = ''
+    words = to_be_translated.split(' / ')
+    
+    for word in words:
+        chars = word.split()
+        for char in chars:
+            if char in morse_codes:
+                final_translation += letters[morse_codes.index(char)]
+        final_translation += ' '
+    
+    print(final_translation.strip())
+
+def main():
+    while True:
+        options = input("""
+1: English to morse code
+2: Morse code to English
+3: Exit the program
+                        
+(1, 2, or 3): """)
+        
+        if options == '1':
+            english_to_morse()
+        elif options == '2':
+            morse_to_english()
+        elif options == '3':
+            print('Thanks for using the program.')
             break
         else:
-            print('Invalid characters found. Please only use ./- and spaces.')
+            print('Incorrect option, try again.')
 
-    # Remove extra spaces and split by '/'
-    to_be_translated = to_be_translated.strip().replace('  ', ' ').split(' ')
-
-    final_translation = ''
-
-    for x in to_be_translated:
-        if x in reversed_translation_dict:
-            final_translation += reversed_translation_dict[x]
-        elif x == '/':
-            final_translation += ' '
-
-    print("English Translation:", final_translation.strip())
-
-
-# Test the functions
-while True:
-    mode = input("Choose mode:\n1. Translate English to Morse Code\n2. Translate Morse Code to English\n3. Quit\n")
-    if mode == '1':
-        english_to_morse()
-    elif mode == '2':
-        morse_to_english()
-    elif mode == '3':
-        print("Goodbye!")
-        break
-    else:
-        print("Invalid choice, please try again.")
+main()
