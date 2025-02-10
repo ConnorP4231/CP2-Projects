91cafc6fa0919bc0acd3009b73d304005b16ab59
ConnorP4231 <connor.pavicic@ucas-edu.net>
2025-02-10 13:03:11 -0700

    idk what i changed

--- test/main.py
+++ test/main.py
@@ -1,83 +1,137 @@
-# Connor Pavicic, morse_code_translator
+#Evan McCabe Raidd: Number Guessing Game
 
-translation_dict = {
-    'a': '.-',    'b': '-...',  'c': '-.-.',  'd': '-..',   'e': '.', 
-    'f': '..-.',  'g': '--.',   'h': '....',  'i': '..',    'j': '.---', 
-    'k': '-.-',   'l': '.-..',  'm': '--',    'n': '-.',    'o': '---', 
-    'p': '.--.',  'q': '--.-',  'r': '.-.',   's': '...',  't': '-', 
-    'u': '..-',   'v': '...-', 'w': '.--',   'x': '-..-',  'y': '-.--', 
-    'z': '--..',  
+# The goal of this project is to generate a number 1-10, have the user guess a number 1-10,
+# and check if they match. It needs to choose a number, let the user guess a number, and then
+# check if the two are identical. If they are, the program should tell them, and ask if they want
+# to play again. Otherwise, the program should let them keep trying until they get it. Once
+# the user guesses correctly, the program should tell them how many tries it took them.
 
-    '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', 
-    '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.'
-}
+# (Psuedocode was written, and then converted to actual code.)
 
-reversed_translation_dict = {
-    '.-': 'a',   '-...': 'b',   '-.-.': 'c',   '-..': 'd',   '.': 'e', 
-    '..-.': 'f', '--.': 'g',   '....': 'h',   '..': 'i',    '.---': 'j', 
-    '-.-': 'k',  '.-..': 'l',  '--': 'm',    '-.': 'n',    '---': 'o', 
-    '.--.': 'p', '--.-': 'q',  '.-.': 'r',   '...': 's',   '-': 't', 
-    '..-': 'u',  '...-': 'v',  '.--': 'w',   '-..-': 'x',  '-.--': 'y', 
-    '--..': 'z',  
+#import random for number generation:
+import random
 
-    '-----': '0', '.----': '1', '..---': '2', '...--': '3', '....-': '4', 
-    '.....': '5', '-....': '6', '--...': '7', '---..': '8', '----.': '9'
-}
+#Make a list of valid guesses for the user to make and make sure it's globally available:
+global validGuesses
+validGuesses = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+#make the user's guesses globally accesible:
+global guesses
+#Set the user's intial number of guesses:
+guesses = 0
+
+#Intro Message:
+print("\nHello! Welcome to the Number Guessing Game. \nYour goal is to guess which number 1 through 10 that I generate.")
 
 
-def english_to_morse():
-    valid_chars = "abcdefghijklmnopqrstuvwxyz1234567890 "
-    while True:
-        to_be_translated = input('What do you want to translate into Morse code? (Only letters, digits, and spaces allowed): ')
-        if all(char in valid_chars for char in to_be_translated):
-            break
-        else:
-            print('Invalid characters found. Please only use letters, digits, and spaces.')
+#function for running the game:
+def runGame():
+    #Make sure the valid guesses variable can be accessed:
+    global validGuesses
+    #Make sure the guesses variable can be accessed:
+    global guesses
+    #Initialize the user's number of guesses to 0:
+    guesses = 0
+    #Pick the number for the user to guess:
+    numToGuess = random.randint(1, 10)
+    #Allow the user to guess the number:
+    guess = input("\nI picked a number!\nWhat is your guess? ")
+    #If the user's guess is not a number:
+    try:
+        1<= int(guess) <= 10
+    except ValueError:
+        guess = input("\nThat is an invalid input. Please try again. ")
+    #if the input is invalid (not a number 1-10):
+    if int(guess) not in validGuesses:
+        guess = input("\nThat is an invalid input. Please try again. ")
     
-    final_translation = ''
-
-    for x in to_be_translated:
-        if x in translation_dict:
-            final_translation += translation_dict[x] + ' '
-        elif x == ' ':
-            final_translation += '/ '  # This represents space in Morse code
-
-    print("Morse Code Translation:", final_translation.strip())
-
-
-def morse_to_english():
-    valid_chars = './- '
-    while True:
-        to_be_translated = input('What do you want to translate into English? (Only Morse code symbols ./- and spaces allowed): ')
-        # Check that all characters in the input are valid Morse symbols or spaces
-        if all(char in valid_chars for char in to_be_translated):
-            break
+    #if the user is right(less likely):
+    if int(guess) == numToGuess:
+        #Tell the user they've won, how many guesses they took, and ask if they'd like to play again:
+        playAgain = input("\nCorrect! You win! XD\nYou guessed in 1 attempt.\n\nPlay again? (y for yes, n for no)\n")
+        #if the user DOES want to play again:
+        if playAgain == "y":
+            #run the game again:
+            runGame()
+        #if the user DOES NOT want to play again:
         else:
-            print('Invalid characters found. Please only use ./- and spaces.')
-
-    # Remove extra spaces and split by '/'
-    to_be_translated = to_be_translated.strip().replace('  ', ' ').split(' ')
-
-    final_translation = ''
-
-    for x in to_be_translated:
-        if x in reversed_translation_dict:
-            final_translation += reversed_translation_dict[x]
-        elif x == '/':
-            final_translation += ' '
-
-    print("English Translation:", final_translation.strip())
-
-
-# Test the functions
-while True:
-    mode = input("Choose mode:\n1. Translate English to Morse Code\n2. Translate Morse Code to English\n3. Quit\n")
-    if mode == '1':
-        english_to_morse()
-    elif mode == '2':
-        morse_to_english()
-    elif mode == '3':
-        print("Goodbye!")
-        break
+            print("\nThanks for playing!\n")
+            #stop the program:
+            exit()
+    #if the user is wrong(more likely):
     else:
-        print("Invalid choice, please try again.")
+        #Tell the user they got it wrong:
+        print("\nWrong! DX")
+        #add 1 to the number of user guesses:
+        guesses += 1
+
+        #Keep letting the user guess the number until they get it right:
+        def wrong():
+            #Make sure the valid guesses variable can be accessed:
+            global validGuesses
+            #Make sure the guesses variable can be accessed:
+            global guesses
+            #Add 1 to the number of guesses if the user is wrong:
+            guesses += 1
+            guess = input("What is your next guess? ")
+            #If the user's guess is not a number:
+            try:
+              1<= int(guess) <= 10
+            except ValueError:
+              guess = input("\nThat is an invalid input. Please try again. ")
+            #if the input is invalid (not a number 1-10):
+            if int(guess) not in validGuesses:
+              guess = input("\nThat is an invalid input. Please try again. ")
+            #Check if the user got it right this time:
+            #if they DID:
+            if int(guess) == numToGuess:
+                #Make sure that the number of guesses can be used in a print statement:
+                guesses = str(guesses)
+                #Tell the user they won, how many guesses they took, and ask if they want to play again:
+                playAgain = input("\nCorrect! You win! XD\nYou guessed in " + guesses + " attempts.\n\nWould you like to play again (y for yes , n for no)\n")
+                if playAgain == "y":
+                #run the game again:
+                  runGame()
+                #if the user DOES NOT want to play again:
+                else:
+                  print("\nThanks for playing!\n")
+                  #stop the program:
+                  exit()
+            #if they DID not guess correctly:
+            else:
+                #Tell the user they were wrong:
+                print("\nWrong!")
+                #Add 1 to the number of user guesses:\
+                guesses += 1
+                #run the function again so the user can keep guessing:
+                wrong()
+        #run the function to let the user try again:
+        wrong()
+
+#run the game:
+runGame()
+
+#Tester #1: Sam
+#Does the program run?
+# yes
+#Did you do something that made the program stop working, if so then what?
+# i guessed the number
+#Were you able to use the program without any help from the programmer?
+# yes
+#If you needed further explanation on things, what explanations did you need?
+# n/a
+
+#Tester #2: 
+#Does the program run?
+# Yes.
+#Did you do something that made the program stop working, if so then what?
+# Winning made the program stop. Oh wait, no it didn't.
+#Were you able to use the program without any help from the programmer?
+# Yes, I was.
+#If you needed further explanation on things, what explanations did you need?
+# I did not need further explanation on anything.
+
+#Improvements made after feedback:
+# I clarified that the computer was generating a number 1 through 10
+# I fixed a small error that caused the number of guesses printed to be incorrect
+# I made sure that the code would keep running smoothly if the user made an invalid input.
