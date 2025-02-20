4856a0e074a07c6429afd2651b6cc578396688cb
ConnorP4231 <connor.pavicic@ucas-edu.net>
2025-02-20 13:29:34 -0700

    do this at home

--- to_do_list/main.py
+++ to_do_list/main.py
@@ -14,11 +14,32 @@ def add_item():
         to_be_appended = task + ": Not done yet."
         file.write("\n"+to_be_appended)
 
-def mark_as_complete():
-    to_be_replaced = input('What text do you want to replace?: ')
-    to_be_replaced_with = input(f'What do you want to replace {to_be_replaced} with?: ')
-     
+def replace():
+    with open("to_do_list/main.txt", "r+") as file:
+        main_txt = file.readlines()
+        to_be_replaced = input('What text do you want to replace?: ')
+        to_be_replaced_with = input(f'What do you want to replace {to_be_replaced} with?: ')
+
+        n = 0
+        for i in main_txt:
+            if to_be_replaced in i:
+                confirm = input(f'Are you sure you want to replace {i} with {to_be_replaced_with}? (yes or no)').lower().strip()
+                if confirm == 'yes':
+                    replacement = i.replace(to_be_replaced, to_be_replaced_with)
+
+                    main_txt[n] = replacement
+                elif confirm == 'no':
+                    break
+                else:
+                    print('Incorrect option, exiting the replace feature.')
+                    break
+            n += 1
+
+replace()
+
+    
 
 with open("to_do_list/main.txt", "r", newline="") as file:
-    reader = file.read()
-    print(reader)
+    contents = file.read()
+
+print(contents)
