81095b66498586d9b6e302a3d510071f3e56e5ba
ConnorP4231 <connor.pavicic@ucas-edu.net>
2025-03-30 16:26:29 -0600

    done with code

--- coin_change/conversion.py
+++ coin_change/conversion.py
@@ -177,5 +177,3 @@ def conversion():
             break
         else:
             print('Incorrect option, try again.')
-
-conversion()
--- coin_change/main.py
+++ coin_change/main.py
@@ -1,12 +1,13 @@
-from conversion import conversion
-from display_csv import display_csv
+from conversion import conversion #Conversion function
+from display_csv import display_csv #Display csv function.
 
-def main():
-    print('This is a program that can tell you the least amount of bills/coins needed for a certain amount of a certain currency, and it also tells you those bills/coins.')
+def main(): #Main function.
+    print('This is a program that can tell you the least amount of bills/coins needed for a certain amount of a certain currency, and it also tells you those bills/coins.') #Describes what the program does.
         
     while True:
-        choice = input('\nWhich one would you like to do?:\n\n 1. Find the lowest amount of coins/bills of a certain amount of a certain currency. \n2. Look at the currency csv file.\n3. Exit the program. \n\n(1, 2, or 3): ')
+        choice = input('\nWhich one would you like to do?:\n\n1. Find the lowest amount of coins/bills of a certain amount of a certain currency. \n2. Look at the currency csv file.\n3. Exit the program. \n\n(1, 2, or 3): ')
 
+        #Does something based on the user input.
         if choice == '1':
             conversion()
         elif choice == '2':
@@ -14,5 +15,8 @@ def main():
         elif choice == '3':
             print('Thanks for using this program.')
             break
-        else:
+        else: #Tells the user they input something wrong if they did.
             print('Incorrect option, try again.')
+
+if __name__ == '__main__': #Runs main function.
+    main()
